25aa6b1ba2030a8b80f7a03195c3a0bafb1b47d5
Dang Minh Du <minhdu@Dangs-MacBook-Pro.local>
2016-03-06 12:13:49 +0700

    AutoLookAt gun ray

---
+++ Scripts/AutoLookAt.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoLookAt : MonoBehaviour {
+
+	Transform mTransform;
+
+	void Awake () {
+		mTransform = GetComponent<Transform> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		mTransform.LookAt (PlayerController.Instance.HitPoint);
+	}
+}
