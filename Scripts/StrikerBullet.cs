36dc12414ac0c164f17f604ad8559ea9cba51f3e
Dang Minh Du <minhdu@Dangs-MacBook-Pro.local>
2016-03-12 18:25:02 +0700

    Add missing script

---
+++ Scripts/StrikerBullet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrikerBullet : MonoBehaviour {
+
+	public float velocity = 0.25f;
+	Transform mTransform;
+	public GameObject impactPrefab;
+	Collider mCollider;
+
+	void Awake () {
+		mTransform = GetComponent<Transform> ();
+		mCollider = GetComponent<BoxCollider> ();
+	}
+
+	void Update () {
+		mTransform.Translate (Vector3.forward*velocity);
+	}
+
+	void OnCollisionEnter (Collision col) {
+		Instantiate(impactPrefab,  col.contacts [0].point, Quaternion.identity);
+		mCollider.enabled = false;
+		Destroy (gameObject);
+	}
+}
