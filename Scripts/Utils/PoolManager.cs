fa06633452f4a0871151213276e73bf5d7e7648b
Dang Minh Du <du.dm@gnt-global.com>
2016-03-23 16:30:04 +0700

    Pool (undone)

--- Scripts/Utils/PoolManager.cs
+++ Scripts/Utils/PoolManager.cs
@@ -1,12 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public enum PoolTag {
-	NONE,
-	BULLET_HOLE
+	BULLET_HOLE = 0,
+	ZOMBIE
 }
 
 public class PoolManager : Singleton<PoolManager> {
+
+	Dictionary<PoolTag, List<GameObject>> caches = new Dictionary<PoolTag, List<GameObject>>();
+
+	Vector3 hidePos = new Vector3(1000,0,0);
+	public string[] resources;
+
+	public void InitPool (PoolTag tag, int amount) {
+		List<GameObject> objs = new List<GameObject> ();
+		GameObject prefab = RescourcesFactory.Load<GameObject> (resources[(int)tag]);
+		for (int i=0; i<amount; i++) {
+			objs.Add(Instantiate(prefab, hidePos, Quaternion.identity) as GameObject);
+		}
+
+		caches.Add (tag, objs);
+	}
 
 	public void Release (GameObject obj, PoolTag tag) {
 		
