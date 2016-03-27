b0f92c6593bc4d90addcfb16493a28496e3d8798
Dang Minh Du <minhdu@Dangs-MacBook-Pro.local>
2016-03-27 08:35:49 +0700

    Update

---
+++ Scripts/Storages/Models/ModelSerializer.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class ModelSerializer {
+	public abstract int GetID ();
+	public abstract string Serialize ();
+	public abstract void Deserialize (Dictionary<string, object> dict);
+}
